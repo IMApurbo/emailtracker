4f469a50b3623f2027b5d11b83ba3305e34f6464
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-04-03 14:32:04 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emailtracker',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'customtkinter',  # Example of a required package with a minimum version
